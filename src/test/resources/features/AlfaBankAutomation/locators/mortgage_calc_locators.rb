4a0a93c0bdc0aad955d06edb0562f900e07a71db
Baffled <cirnotan9@gmail.com>
2022-04-25 11:41:25 +0300

    make locators immutable

--- src/test/resources/features/AlfaBankAutomation/locators/mortgage_calc_locators.rb
+++ src/test/resources/features/AlfaBankAutomation/locators/mortgage_calc_locators.rb
@@ -1,17 +1,23 @@
+# frozen_string_literal: true
+
 module Locators
+
   class MortgageCalcLocators
-    element :real_estate_cost_textbox, '[name="cost"]'
-    element :mortgage_down_payment_textbox, '[name="start_sum"]'
-    element :rub_currency_or_percentage_dropdown, 'select[name="start_sum_type"]'
-    element :rub_currency_or_percentage_dropdown_select_option, 'select[name="start_sum_type"] > option[value="2"]'
-    element :mortgage_term_textbox, 'input[name="period"]'
-    element :years_or_months_dropdown, 'select[name="period_type"]'
-    element :years_or_months_dropdown_select_option, 'select[name="period_type"] > option[value="Y"]'
-    element :interest_rate_textbox, 'input[name="percent"]'
-    element :annuity_radiobutton, '[for="payment-type-1"]'
-    element :differential_radiobutton, '[for="payment-type-2"]'
-    element :submit_button, 'input[class="calc-submit"]'
-    element :calculation_result_monthly_payment, '.calc-result-value.result-placeholder-monthlyPayment'
+
+    REAL_ESTATE_COST_TEXTBOX = '[name="cost"]'
+    MORTGAGE_DOWN_PAYMENT_TEXTBOX = '[name="start_sum"]'
+    RUB_CURRENCY_OR_PERCENTAGE_DROPDOWN = 'select[name="start_sum_type"]'
+    RUB_CURRENCY_OR_PERCENTAGE_DROPDOWN_SELECT_OPTION_1 = 'select[name="start_sum_type"] > option[value="1"]'
+    RUB_CURRENCY_OR_PERCENTAGE_DROPDOWN_SELECT_OPTION_2 = 'select[name="start_sum_type"] > option[value="2"]'
+    MORTGAGE_TERM_TEXTBOX = 'input[name="period"]'
+    YEARS_OR_MONTHS_DROPDOWN = 'select[name="period_type"]'
+    YEARS_OR_MONTHS_DROPDOWN_SELECT_OPTION_M = 'select[name="period_type"] > option[value="M"]'
+    YEARS_OR_MONTHS_DROPDOWN_SELECT_OPTION_Y = 'select[name="period_type"] > option[value="Y"]'
+    INTEREST_RATE_TEXTBOX = 'input[name="percent"]'
+    ANNUITY_RADIOBUTTON = '[for="payment-type-1"]'
+    DIFFERENTIAL_RADIOBUTTON = '[for="payment-type-2"]'
+    SUBMIT_BUTTON = 'input[class="calc-submit"]'
+    CALCULATION_RESULT_MONTHLY_PAYMENT_TEXT = '.calc-result-value.result-placeholder-monthlyPayment'
 
   end
 end
