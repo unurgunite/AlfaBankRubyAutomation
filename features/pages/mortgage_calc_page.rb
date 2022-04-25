0f6c5905f0f2501baafca4086189b67c3e609b07
unurgunite <senpaiguru1488@gmail.com>
2022-04-26 01:54:42 +0300

    Made specs definition dynamically

--- features/pages/mortgage_calc_page.rb
+++ features/pages/mortgage_calc_page.rb
@@ -3,44 +3,19 @@
 require_relative '../locators/mortgage_calc_locators'
 require 'selenium-webdriver'
 
-class MortgageCalc < Locators::MortgageCalcLocators
-  @@down_payment_should_be = 2_400_000
-  @@loan_sum_should_be = 9_600_000
+class MortgageCalc < Locators::MortgageCalcLocators # :nodoc:
+  @down_payment_should_be = 2_400_000
+  @loan_sum_should_be = 9_600_000
+  specs = %i[header_exists by_estate_price_link_exists by_loan_amount_link_exists estate_price_text_exists
+             down_payment_text_exists loan_sum_text_exists interest_rate_text_exists mortgage_term_text_exists
+             payment_radiobtn_text_exists]
 
-  def header_exists(driver)
-    raise 'header text does not exist' if driver.find_element(:css, HEADER).nil?
-  end
-
-  def by_estate_price_link_exists(driver)
-    raise 'estate price link does not exist' if driver.find_element(:css, BY_PRICE_LINK).nil?
-  end
-
-  def by_loan_amount_link_exists(driver)
-    raise 'loan amount link does not exist' if driver.find_element(:css, BY_LOAN_AMOUNT_LINK).nil?
-  end
-
-  def estate_price_text_exists(driver)
-    raise 'estate price text does not exist' if driver.find_element(:css, ESTATE_PRICE_TEXT).nil?
-  end
-
-  def down_payment_text_exists(driver)
-    raise 'down payment text does not exist' if driver.find_element(:css, DOWN_PAYMENT_TEXT).nil?
-  end
-
-  def loan_sum_text_exists(driver)
-    raise 'loan sum text does not exist' if driver.find_element(:css, LOAN_SUM_TEXT).nil?
-  end
-
-  def interest_rate_text_exists(driver)
-    raise 'interest rate text does not exist' if driver.find_element(:css, INTEREST_RATE_TEXT).nil?
-  end
-
-  def mortgage_term_text_exists(driver)
-    raise 'mortgage term text does not exist' if driver.find_element(:css, MORTGAGE_TERM_TEXT).nil?
-  end
-
-  def payment_radiobtn_text_exists(driver)
-    raise 'radiobutton text does not exist' if driver.find_element(:css, PAYMENT_RADIOBUTTON_TEXT).nil?
+  Locators::MortgageCalcLocators.constants.each do |const|
+    specs.each do |m|
+      define_method m do |driver|
+        m.to_s.split('_').insert(-2, 'does not').join(' ') if driver.find_element(:css, const).nil?
+      end
+    end
   end
 
   def print_real_estate
@@ -67,9 +42,9 @@ class MortgageCalc < Locators::MortgageCalcLocators
     down_payment = filter(down_payment, '0123456789').to_i
     loan_sum = driver.find_element(:css, LOAN_SUM_RESULT).text.gsub(/\s+/, '').to_i
 
-    raise 'Down_payments do not match!' if down_payment.to_i != @@down_payment_should_be
+    raise 'Down_payments do not match!' unless down_payment.to_i == @down_payment_should_be
 
-    raise 'Loan sums do not match!' if loan_sum != @@loan_sum_should_be
+    raise 'Loan sums do not match!' unless loan_sum == @loan_sum_should_be
   end
 
   def enter_mortgage_term(driver, string)
@@ -91,9 +66,9 @@ class MortgageCalc < Locators::MortgageCalcLocators
   end
 
   def select_annuity_radiobutton(driver)
-    if driver.find_element(:css, DIFFERENTIAL_RADIOBUTTON).attribute('checked')
-      raise 'DIFFERENTIAL RADIOBUTTON IS CHECKED'
-    end
+    return unless driver.find_element(:css, DIFFERENTIAL_RADIOBUTTON).attribute('checked')
+
+    raise 'DIFFERENTIAL RADIOBUTTON IS CHECKED'
   end
 
   def click_submit_button(driver)
@@ -111,8 +86,8 @@ class MortgageCalc < Locators::MortgageCalcLocators
     result = result.gsub(/\s+/, '').gsub(',', '.').to_f
     expected = expected.round(2)
 
-    raise 'oof' if result != expected
+    raise 'oof' unless result == expected
 
-    puts '___________TEST OK :)________________'
+    puts '___________TEST OK :)___________'
   end
 end
