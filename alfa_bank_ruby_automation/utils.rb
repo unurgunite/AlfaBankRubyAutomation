c1404830ca23e14c3e7c3bcffa0952994c6ea888
unurgunite <senpaiguru1488@gmail.com>
2022-04-26 01:53:36 +0300

    Refactored source tree

--- alfa_bank_ruby_automation/utils.rb
+++ alfa_bank_ruby_automation/utils.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-class Utils
+class Utils # :nodoc:
   def calculate_result(rate)
     sum = 9_600_000
     i = (rate / 12.0) / 100.0
     period_months = 20 * 12
-    koef = (i * ((i + 1)**period_months)) / (((i + 1)**period_months) - 1)
-    sum * koef
+    coefficient = (i * ((i + 1)**period_months)) / (((i + 1)**period_months) - 1)
+    sum * coefficient
   end
 
   def generate_random_interest_rate
